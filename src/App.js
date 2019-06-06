fc7ad6780db8f4d577577749effc2fe5a1881d79
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-06-06 11:46:44 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to I was misa.
+          Edit <code>src/App.js</code> change to magically upload
         </p>
         <a
           className="App-link"
